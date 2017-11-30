e751ae42d8629bab6238c3e5fa813ac161419222
lutsiy <lutsiy@inbox.ru>
2017-11-30 17:27:45 +0700

    Домашнее задание 1 Полностью разобраться с кодом, попробовать переписать с нуля, стараясь не подглядывать в методичку; 2 Переделать проверку победы, чтобы она не была реализована просто набором условий, например, с использованием циклов. 3 * Попробовать переписать логику проверки победы, чтобы она работала для поля 5х5 и количества фишек 4. Очень желательно не делать это просто набором условий для каждой из возможных ситуаций; 4 *** Доработать искусственный интеллект, чтобы он мог блокировать ходы игрока.

---
+++ Lesson_04/src/Lesson_04/Lesson_04.java
@@ -0,0 +1,15 @@
+/**
+ * Java 1. Home Work 2
+ * @author Ilya Lutsevich
+ * @version dated Nov 28 2017
+ */
+
+
+package Lesson_04;
+
+public class Lesson_04 {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
